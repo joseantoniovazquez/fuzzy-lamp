ba9780b491dfad1e97a4ea160021e28452401105
joseantoniovazquez <joseantonio.vazquez@globant.com>
2021-10-12 22:08:28 -0500

    creando app react hover

--- README.md
+++
@@ -1,5 +0,0 @@
-# fuzzy-lamp
-just a test repo
-CAOS Y DESTRUCCION EL DIA DE HOY
-second chaos
-some fix
---
+++ my-app/src/other.js
@@ -0,0 +1,7 @@
+    <button data-tip data-for="registerTip">
+        Register
+    </button>
+
+    <ReactTooltip id="registerTip" place="top" effect="solid">
+        Tooltip for the register button
+    </ReactTooltip>
